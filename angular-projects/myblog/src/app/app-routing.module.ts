5825a8974db232c179ff28d0a6e08b22e0654e9d
cujardmt <cujardo.moises@gmail.com>
2022-03-19 15:11:14 +0800

    added articles module

--- angular-projects/myblog/src/app/app-routing.module.ts
+++ angular-projects/myblog/src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: 'contact', component: ContactComponent}
+  {path: 'contact', component: ContactComponent},
+  // TODO: Please expound this line
+  //    ng generate articles --route=articles --module=app-routing
+  // TODO: Learn more about Angular Routing
+  { path: 'articles', loadChildren: () => import('./articles/articles.module').then(m => m.ArticlesModule) }
 ];
 
 @NgModule({
