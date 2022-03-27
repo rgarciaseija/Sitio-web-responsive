9d2ae0b82fa57ab53451dd98cf78e7edef0466ac
cujardmt <cujardo.moises@gmail.com>
2022-03-27 16:55:08 +0800

    with routing

--- angular-projects/angular-router-sample/src/app/app.module.ts
+++ angular-projects/angular-router-sample/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CrisisListComponent } from './components/crisis-list/crisis-list.component';
+import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrisisListComponent,
+    HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // define two routes using the RoterModule's
+    // forRoot() function.
+    // forRoot() function ensures that this application
+    // only instantiates one RoterModule. Singleton?
+    RouterModule.forRoot([
+      { path: 'crisis-list', component: CrisisListComponent },
+      { path: 'heroes-list', component: HeroesListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
