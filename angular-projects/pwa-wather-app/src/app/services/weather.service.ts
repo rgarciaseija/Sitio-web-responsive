4bad008b1bdf323f0cff57bbd2b34762c9afc4d8
cujardmt <cujardo.moises@gmail.com>
2022-04-15 19:47:02 +0800

    optional units

--- angular-projects/pwa-wather-app/src/app/services/weather.service.ts
+++ angular-projects/pwa-wather-app/src/app/services/weather.service.ts
@@ -11,16 +11,22 @@ export class WeatherService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getWeather(city: string): Observable<Weather> {
+  getWeather(city: string, units: string = 'metric'): Observable<Weather> {
     const options = new HttpParams()
-    .set('units', 'metric')
+    // https://openweathermap.org/current#data
+    //
+    // we use metric for units to show temperatures in Celcius
+    // use imperial if you want to use Fahrenheit
+    // units is optional. if left blank or not specified,
+    // kelvin will be used by default
+    .set('units', units)
     .set('q', city)
     .set('appId', environment.apiKey)
 
     // reference:
     //  https://openweathermap.org/current
     //  https://openweathermap.org/current#data
-    console.log(environment.apiUrl+'weather');
+    // console.log(environment.apiUrl+'weather');
 
     return this.httpClient.get<Weather>(environment.apiUrl+'weather', {params: options});
   }
